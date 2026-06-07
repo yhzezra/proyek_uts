0f0bd5dd660703e68fa145844d7c59b0d5c6b9a5
yhzezra <yhzezra15@gmail.com>
2026-06-07 23:59:16 +0700

    Add files via upload

---
+++ MejaKantor.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/MejaKantor.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo"> GALERI<span class="warna">FURNITURE</span></div> 
+        <ul class="nav-links">
+            <li><a href="index.php" class="testbtn">Beranda</a></li>
+            <li><a href="#" class="testbtn" id="katalogBtn">Katalog</a></li>
+            <li><a href="Tentang.php" class="testbtn">Tentang</a></li>
+            <li><a href="Kontak.php" class="testbtn">Kontak</a></li>
+        </ul>
+    </nav>
+
+    <div class="popup" id="popupKatalog">
+        <ul class="popupbtn">
+            <li><a href="MejaMakan.php" class="popupbtn">Meja Makan</a></li>
+            <li><a href="MejaKantor.php" class="popupbtn">Meja Kantor</a></li>
+            <li><a href="Kasur.php" class="popupbtn">Kasur</a></li>
+            <li><a href="RakSepatu.php" class="popupbtn">Rak Sepatu</a></li>
+        </ul>
+    </div>
+
+      <hr class="divider">
+
+            <div class="product-grid">
+            <div class="product-card">
+                <img src="Aset/MK - 1.jpeg" class="product-grid">
+                <h3>MT AZTEC</h3>
+                <p>1200 x 600 x 750 cm</p>
+                <p>Warna: Sonoma Terang</p>
+            </div>
+
+            <div class="product-card">
+                <img src="Aset/MK - 2.jpeg" alt="MT 120 White Sonoma" />
+                <h3>MT MAYA</h3>
+                <p>1200 x 600 x 960 cm</p>
+                <p>Warna: Sonoma Terang</p>
+            </div>
+
+             <div class="product-card">
+                <img src="Aset/MK - 3.jpeg" alt="MT 120 White Sonoma" />
+                <h3>MT MAYA</h3>
+                <p>1200 x 600 x 960 cm</p>
+                <p>Warna: Coklat Walnut </p>
+            </div>
+
+            <div class="product-card">
+                <img src="Aset/MK - 4.jpeg" alt="MT 120 White Sonoma" />
+                <h3>MT MAYA</h3>
+                <p>1200 x 600 x 960 cm</p>
+                <p>Warna: Ceri</p>
+            </div>
+        </div>
+        <script src="JS/Katalog.js"></script>
+</body>
+</html>
---
+++ Tentang.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Galeri Furniture</title>
+    <link rel="stylesheet" href="CSS/Tentang.css"/>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo"> GALERI<span class="warna">FURNITURE</span></div>
+        </div> <ul class="nav-links">
+            <li><a href="index.php" class="testbtn">Beranda</a></li>
+            <li><a href="#" class="testbtn" id="katalogBtn">Katalog</a></li>
+            <li><a href="Tentang.php" class="testbtn">Tentang</a></li>
+            <li><a href="Kontak.php" class="testbtn">Kontak</a></li>
+        </ul>
+    </nav>
+
+    <hr class="divider">
+
+    <div class="popup" id="popupKatalog">
+        <ul class="popupbtn">
+            <li><a href="MejaMakan.php" class="popupbtn">Meja Makan</a></li>
+            <li><a href="MejaKantor.php" class="popupbtn">Meja Kantor</a></li>
+            <li><a href="Kasur.php" class="popupbtn">Kasur</a></li>
+            <li><a href="RakSepatu.php" class="popupbtn">Rak Sepatu</a></li>
+        </ul>
+    </div>
+
+    <section class="tentang">
+        <div class="header">
+            <h1>Tentang Kami</h1>
+            <h3>Temukan Interior Terbaik Untuk Rumah Anda</h3>
+        </div>
+        <div class="tentang-wrapper">
+            <div class="image">
+                <img src="Aset/Foto-toko.jpeg" alt="">
+            </div>
+
+            <div class="text">
+                <p>Galeri kami bukan tentang membuat furniture, melainkan tentang menemukan, memilih, dan menghadirkan karya terbaik ke dalam ruang yang tepat. 
+                Berawal dari ketertarikan terhadap desain dan kualitas, kami membangun galeri ini sebagai tempat di mana setiap orang dapat menemukan furniture 
+                yang tidak hanya indah secara visual, tetapi juga nyaman dan fungsional. Melalui proses kurasi yang cermat, kami bekerja sama dengan berbagai brand 
+                dan produsen terpercaya untuk memastikan setiap koleksi memiliki standar tinggi. Seiring waktu, galeri ini berkembang menjadi ruang inspirasi, di mana 
+                pelanggan dapat membayangkan dan mewujudkan ruang impian mereka. Kami percaya bahwa furniture adalah bagian dari identitas dan gaya hidup, sehingga s
+                etiap pilihan memiliki makna, dan dengan komitmen untuk terus berkembang, kami berupaya menghadirkan koleksi yang relevan, berkualitas, serta memberikan 
+                pengalaman terbaik bagi setiap pelanggan.
+                </p>
+            </div>
+        </div>
+    </section>
+    <script src="JS/Katalog.js"></script>
+    </body>
+</html>
