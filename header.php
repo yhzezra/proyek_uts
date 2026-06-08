5d8fab4c31cce0f874b3bb976663948d32090f5e
yhzezra <yhzezra15@gmail.com>
2026-06-08 17:51:32 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,34 @@
-<?php
-include "koneksi.php";
+<?php 
+include 'koneksi.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo"> GALERI<span class="warna">FURNITURE</span></div>
+        </div> <ul class="nav-links">
+            <li><a href="index.php" class="testbtn">Beranda</a></li>
+            <li><a href="#" class="testbtn" id="katalogBtn">Katalog</a></li>
+            <li><a href="Tentang.php" class="testbtn">Tentang</a></li>
+            <li><a href="Kontak.php" class="testbtn">Kontak</a></li>
+        </ul>
+    </nav>
+
+    <hr>
+
+    <div class="popup" id="popupKatalog">
+        <ul class="popupbtn">
+            <li><a href="MejaMakan.php" class="popupbtn">Meja Makan</a></li>
+            <li><a href="MejaKantor.php" class="popupbtn">Meja Kantor</a></li>
+            <li><a href="Kasur.php" class="popupbtn">Kasur</a></li>
+            <li><a href="RakSepatu.php" class="popupbtn">Rak Sepatu</a></li>
+        </ul>
+    </div>
+</body>
+</html>
