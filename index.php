1cf45421b48f3c7b7a3ab5a6aa8bf1cef5fa2fdb
yhzezra <yhzezra15@gmail.com>
2026-06-08 18:06:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+ include "header.php";
+?>
+
 <!doctype html>
 <html lang="id">
 <head>
@@ -7,27 +11,6 @@
     <link rel="stylesheet" href="CSS/Beranda.css" />
 </head>
 <body>
-    <nav class="navbar">
-        <div class="logo"> GALERI<span class="warna">FURNITURE</span></div>
-        </div> <ul class="nav-links">
-            <li><a href="index.php" class="testbtn">Beranda</a></li>
-            <li><a href="#" class="testbtn" id="katalogBtn">Katalog</a></li>
-            <li><a href="Tentang.php" class="testbtn">Tentang</a></li>
-            <li><a href="Kontak.php" class="testbtn">Kontak</a></li>
-        </ul>
-    </nav>
-
-    <hr class="divider">
-
-    <div class="popup" id="popupKatalog">
-        <ul class="popupbtn">
-            <li><a href="MejaMakan.php" class="popupbtn">Meja Makan</a></li>
-            <li><a href="MejaKantor.php" class="popupbtn">Meja Kantor</a></li>
-            <li><a href="Kasur.php" class="popupbtn">Kasur</a></li>
-            <li><a href="RakSepatu.php" class="popupbtn">Rak Sepatu</a></li>
-        </ul>
-    </div>
-
     <header class="home">
         <div class="home-content">  
             <h1>Koleksi Interior Terbaru</h1>
