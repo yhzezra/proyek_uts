c94f3e7fc45fd0cb282b21d48eaf0de38134ea6f
Louisa Ferdinand <louisalouisa842@gmail.com>
2026-06-17 23:21:00 +0700

    Create hapus.php

---
+++ Admin/kategori/hapus.php
@@ -0,0 +1,27 @@
+$id = $_GET['id'];
+
+$cek = mysqli_query(
+    $koneksi,
+    "SELECT * FROM produk
+     WHERE id_kategori='$id'"
+);
+
+if(mysqli_num_rows($cek) > 0){
+
+    echo "
+    <script>
+        alert('Kategori tidak dapat dihapus karena masih memiliki produk');
+        window.location='index.php';
+    </script>
+    ";
+
+    exit;
+}
+
+mysqli_query(
+    $koneksi,
+    "DELETE FROM kategori
+     WHERE id_kategori='$id'"
+);
+
+header("Location:index.php");
