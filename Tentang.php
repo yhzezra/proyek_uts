513f5f19ec259bc5da6991577ce8a4e50d1e0b68
Carren Cynthia Arlie <ccarrenarlie@gmail.com>
2026-07-03 00:03:56 +0700

    Update Tentang.php

--- Tentang.php
+++ Tentang.php
@@ -9,6 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Galeri Furniture</title>
     <link rel="stylesheet" href="CSS/Tentang.css"/>
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
 </head>
 <body>
     <section class="tentang">
